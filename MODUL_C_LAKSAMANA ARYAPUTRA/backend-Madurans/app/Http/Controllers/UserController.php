99cb02e36d0f854d150fa01c19c7ae6aebff85ff
laksamanaap <laksamana.arya1412@gmail.com>
2023-11-20 23:06:28 +0700

    feat : setup swagger

---
+++ MODUL_C_LAKSAMANA ARYAPUTRA/backend-Madurans/app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+
+    /**
+     * @OA\Get(
+     *     path="/get-users",
+     *     tags={"Users"},
+     *     summary="Returns a Sample API response",
+     *     description="A sample greeting to test out the API",
+     *     operationId="greet",
+     *     @OA\Parameter(
+     *          name="firstname",
+     *          description="nama depan",
+     *          required=true,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *     ),
+     *     @OA\Parameter(
+     *          name="lastname",
+     *          description="nama belakang",
+     *          required=true,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="successful operation"
+     *     )
+     * )
+     */
+    public function index() {
+      $users = DB::table('users')->get();  
+      return response()->json($users);
+    } 
+}
